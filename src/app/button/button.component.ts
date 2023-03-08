3032da6149456b3982fd52efca570ac922bcdd04
Chris Northcutt <chris.r.northcutt@gmail.com>
2023-03-08 12:17:52 -0500

    Fixes

--- src/app/button/button.component.ts
+++ src/app/button/button.component.ts
@@ -1,14 +1,11 @@
 import { Component, Input, OnInit } from '@angular/core';
-import { MatButton } from '@angular/material/button';
 
 @Component({
   selector: 'app-button',
   templateUrl: './button.component.html',
   styleUrls: ['./button.component.scss'],
 })
-
-// Material Button
-export class ButtonComponent{
+export class ButtonComponent {
   @Input()
   Label: string;
   @Input()
@@ -19,54 +16,4 @@ export class ButtonComponent{
   iconLeft: boolean;
   @Input()
   iconRight: boolean;
-  // @Input()
-  // color: string;
-  // @Input()
-  // disabled: boolean;
 }
-// @Component({
-//   selector: 'button[mat-stroked-button]',
-//   templateUrl: './button.component.html',
-//   styleUrls: ['./button.component.scss'],
-// })
-
-// Material Button
-// export class ButtonStrokedComponent{
-//   @Input()
-//   Label: string;
-//   @Input()
-//   Type: string[] = ['mat-raised-button', 'mat-stroked-button', 'mat-button'];
-//   @Input()
-//   Disabled: boolean;
-//   @Input()
-//   iconLeft: boolean;
-//   @Input()
-//   iconRight: boolean;
-//   @Input()
-//   color: string;
-//   @Input()
-//   disabled: boolean;
-// }
-
-// @Component({
-//   selector: 'button[mat-button]',
-//   templateUrl: './button.component.html',
-//   styleUrls: ['./button.component.scss'],
-// })
-// // Material Button
-// export class ButtonBasicComponent{
-//   @Input()
-//   Label: string;
-//   @Input()
-//   Type: string[] = ['mat-raised-button', 'mat-stroked-button', 'mat-button'];
-//   @Input()
-//   Disabled: boolean;
-//   @Input()
-//   iconLeft: boolean;
-//   @Input()
-//   iconRight: boolean;
-//   @Input()
-//   color: string;
-//   @Input()
-//   disabled: boolean;
-// }
